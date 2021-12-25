be11e197c3fddaf18db0ea39172ce2d59a6628c3
Janis Jansen <janis.jansen@gmx.de>
2021-12-25 12:43:47 +0100

    started to implement pointcloud rendering

--- src/components/Three/Pointcloud/Pointcloud.tsx
+++ src/components/Three/Pointcloud/Pointcloud.tsx
@@ -1,8 +1,11 @@
+import { PointMaterial } from "@react-three/drei";
 import { useEffect, useState } from "react";
 import { loadHdf5, reshapeArray } from "utils";
 
 export default function Pointcloud() {
   const [file, setFile] = useState<any>();
+  const [points, setPoints] = useState([]);
+  const [colors, setColors] = useState<number[]>([]);
 
   useEffect(() => {
     loadHdf5("./assets/hdf5/1.hdf5").then((f) => {
@@ -16,14 +19,40 @@ export default function Pointcloud() {
       const fkeys = file.keys();
 
       fkeys.forEach((key: string) => {
-        const dataset = file.get(key);
-        const dim = dataset.shape[1] || 1;
-        rows[key] = reshapeArray(dataset.value, dim);
+        if (key === "points") {
+          const dataset = file.get(key);
+
+          const cols = [];
+          for (let index = 0; index < dataset.value.length; index++) {
+            cols.push(0.5);
+          }
+          setColors(cols);
+          setPoints(dataset.value);
+          // const dim = dataset.shape[1] || 1;
+
+          // rows[key] = reshapeArray(dataset.value, dim);
+        }
       });
-      console.log(rows);
-      console.log(fkeys);
     }
   }, [file, setFile]);
 
-  return <mesh></mesh>;
+  useEffect(() => {
+    console.log(points);
+    console.log(colors);
+  }, [colors, points]);
+
+  return (
+    <points>
+      <bufferGeometry attach="geometry">
+        <bufferAttribute
+          attachObject={["attributes", "position"]}
+          count={points.length / 3}
+          array={points}
+          itemSize={3}
+        />
+        <bufferAttribute attachObject={["attributes", "color"]} count={colors.length / 3} array={colors} itemSize={3} />
+      </bufferGeometry>
+      <PointMaterial />
+    </points>
+  );
 }
